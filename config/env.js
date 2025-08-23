d988c48a22e3d7d159c3690c49602754875d4ada
Aneeshhegde <aneeshhegde33@gmail.com>
2025-08-23 16:34:20 +0530

    Delete config directory

--- config/env.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const port = process.env.PORT || 4000;
-export const mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/krishilink';
-export const nodeEnv = process.env.NODE_ENV || 'development';
